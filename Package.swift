9515553724e80a26240bc984819678bd1d3d250b
kjhhaha@gmail.com <JeongHyun1!>
2024-03-16 00:59:48 +0900

    지원 Os 추가작업

--- Package.swift
+++ Package.swift
@@ -6,7 +6,9 @@ import PackageDescription
 let package = Package(
     name: "LevelOSLog",
     platforms: [
-        .iOS(.v13)
+        .iOS(.v13),
+        .watchOS(.v7),
+        .macOS(.v11)
     ], products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
